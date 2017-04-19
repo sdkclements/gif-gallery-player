4c16b8acfa24e75d755977349efc9aae3018a96a
Samuel Clements <sdkclements@gmail.com>
2017-04-19 14:42:15 -0400

    center homescreen

--- src/scenes/welcome.js
+++ src/scenes/welcome.js
@@ -4,13 +4,14 @@ import { Jumbotron, Button } from 'react-bootstrap'
 import classnames from 'classnames'
 import GifPlayer from 'react-gif-player'
 import giff from '../../src/images/giphy.gif'
+import './welcome.css'
 
 export default class Welcomeview extends Component {
     render(){
         const { className, ...props } = this.props;
         return(
             <div className={ classnames('Welcomeview', className) } {...props}>
-                <Jumbotron>
+                <Jumbotron className="welcomescrn">
                     <h1>Welcome to the Gif Gallery Creator! </h1>
                     <p>Upload, Search and make lists of Gifs .</p>
                     <p><Button bsStyle="info"><Link to="/search"> Add em up</Link></Button></p>
